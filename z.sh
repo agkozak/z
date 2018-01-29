4a9cd9db715e09038957ef00c0d5f92c22042341
Alexandros Kozak <git-agkozak@sneakemail.com>
2018-01-28 22:18:16 -0800

    Avoid add-zsh-hook

--- z.sh
+++ z.sh
@@ -237,8 +237,7 @@ if type compctl >/dev/null 2>&1; then
                 (_z --add "${PWD:A}" &)
             }
         fi
-        autoload -Uz add-zsh-hook
-        add-zsh-hook -Uz chpwd _z_chpwd
+      chpwd_functions+=(_z_chpwd) 
     }
     _z_zsh_tab_completion() {
         # tab completion
